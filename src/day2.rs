cff4f11cc6d0d1e06e64e5d4c145a55007f88016
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-09 11:18:53 +0100

    Combine `day2-1` and `day2-2` into top-level project

--- src/day2.rs
+++ src/day2.rs
@@ -1,9 +1,6 @@
 use anyhow::{anyhow, Context};
 use std::str::FromStr;
 
-#[cfg(test)]
-use claim::assert_ok_eq;
-
 #[derive(Debug)]
 enum Action {
     Forward(u32),
@@ -39,18 +36,41 @@ impl FromStr for Action {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let (horizontal, depth) = calc_position(include_str!("input.txt"))?;
+pub fn run() -> anyhow::Result<()> {
+    let (horizontal, depth) = calc_position(include_str!("../inputs/input-2.txt"))?;
     dbg!(horizontal);
     dbg!(depth);
 
-    let solution = horizontal * depth;
-    dbg!(solution);
+    let result_1 = horizontal * depth;
+    dbg!(result_1);
+
+    let (horizontal, depth) = calc_position2(include_str!("../inputs/input-2.txt"))?;
+    dbg!(horizontal);
+    dbg!(depth);
+
+    let result_2 = horizontal * depth;
+    dbg!(result_2);
 
     Ok(())
 }
 
 fn calc_position(input: &str) -> anyhow::Result<(u32, u32)> {
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    for line in input.lines() {
+        let action: Action = line.parse()?;
+        match action {
+            Action::Forward(num) => horizontal += num,
+            Action::Down(num) => depth += num,
+            Action::Up(num) => depth -= num,
+        }
+    }
+
+    Ok((horizontal, depth))
+}
+
+fn calc_position2(input: &str) -> anyhow::Result<(u32, u32)> {
     let mut horizontal = 0;
     let mut depth = 0;
     let mut aim = 0;
@@ -70,7 +90,17 @@ fn calc_position(input: &str) -> anyhow::Result<(u32, u32)> {
     Ok((horizontal, depth))
 }
 
-#[test]
-fn test() {
-    assert_ok_eq!(calc_position(include_str!("example.txt")), (15, 60));
-}
+#[cfg(test)]
+mod tests {
+    use claim::assert_ok_eq;
+    use super::{calc_position,calc_position2};
+
+    #[test]
+    fn part_1() {
+        assert_ok_eq!(calc_position(include_str!("../inputs/example-2.txt")), (15, 10));
+    }
+
+    #[test]
+    fn part_2() {
+        assert_ok_eq!(calc_position2(include_str!("../inputs/example-2.txt")), (15, 60));
+    }}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod day1;
+mod day2;
 
 fn main() -> anyhow::Result<()> {
     day1::run()?;
+    println!();
+
+    day2::run()?;
 
     Ok(())
 }
